c23cab62f4e0b1a4acc6ea731aeddd2e915da99c
igmerwina <igmerwina@gmail.com>
2019-04-10 13:28:02 +0700

    Add expressjs

---
+++ dev/api.js
@@ -0,0 +1,9 @@
+let express = require('express')
+let app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
+console.log('go to http://localhost:3000')
