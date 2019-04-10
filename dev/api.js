4dba99d908612f5e4c705de1fb2bae20e9df95cf
igmerwina <igmerwina@gmail.com>
2019-04-10 14:21:21 +0700

    adadd: createNewTransaction on /transaction

--- dev/api.js
+++ dev/api.js
@@ -17,8 +17,8 @@ app.get('/blockchain', function (req, res) {
 
 // end-point to create new transaction
 app.post('/transaction', function (req, res) {
-    console.log(req.body)
-    res.send(`The amount of the transaction is ${req.body.amount} bitcoin`)
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient)
+    res.json({ note: `Transaction will be added in block ${blockIndex}.` })
 })
 
 
